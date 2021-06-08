0cf42c74f855d8ac13afa8f45bc344d7051d6054
Guilherme <guilherme.gg1@hotmail.com>
2021-06-08 19:55:16 -0300

    hdri and sample materials

--- objects3d/GLTFWalls/GLTFWalls.js
+++ objects3d/GLTFWalls/GLTFWalls.js
@@ -1,9 +1,13 @@
-import { useMemo } from 'react'
+import { useRef, useEffect, useMemo } from 'react'
 import * as THREE from 'three'
-import { useGLTF, Box } from '@react-three/drei'
+import { useGLTF, useHelper } from '@react-three/drei'
+import { useLoader } from '@react-three/fiber'
 import { useBox } from '@react-three/cannon'
+import { VertexNormalsHelper } from 'three-stdlib'
 
-const Wall = ({ geometry, material, showCollisions }) => {
+const Wall = ({ children, material, node, geometry, showCollisions }) => {
+  const mesh = useRef()
+
   const [args, position] = useMemo(() => {
     const { boundingBox } = geometry
     const box = new THREE.Box3(boundingBox.min, boundingBox.max)
@@ -15,20 +19,47 @@ const Wall = ({ geometry, material, showCollisions }) => {
 
   useBox(() => ({ position, args, type: 'Static' }))
 
+  // useHelper(mesh, THREE.BoxHelper, 'yellow')
+  // useHelper(mesh, VertexNormalsHelper, 1, 'green')
+
   return (
-    <>
-      <mesh geometry={geometry} material={material} castShadow />
-      <Box position={position} args={args} visible={showCollisions}>
-        <meshBasicMaterial color="red" wireframe />
-      </Box>
-    </>
+    <mesh ref={mesh} geometry={geometry} material={node.material} castShadow>
+      {children}
+    </mesh>
   )
 }
 
 const GLTFWalls = ({ path, showCollisions }) => {
   const { nodes, materials } = useGLTF(path)
 
+  // const [base, bump, normal, ao, rough] = useLoader(THREE.TextureLoader, [
+  //   '/materials/grass/basecolor.jpg',
+  //   '/materials/grass/height.png',
+  //   '/materials/grass/normal.jpg',
+  //   '/materials/grass/ambientOcclusion.jpg',
+  //   '/materials/grass/roughness.jpg',
+  // ])
+
+  // useEffect(() => {
+  //   const repeatX = 0.015
+  //   const repeatY = 0.015
+  //   ;[base, bump, normal, ao, rough].forEach((texture) => {
+  //     texture.wrapS = THREE.RepeatWrapping
+  //     texture.wrapT = THREE.RepeatWrapping
+  //     texture.repeat.set(repeatX, repeatY)
+  //     console.log(32323, texture)
+  //   })
+  // }, [base, bump, normal, ao, rough])
+
   const idsNodes = useMemo(() => Object.keys(nodes), [nodes])
+
+  // useEffect(() => {
+  //   if (materials?.Granite_) {
+  //     materials.Granite_.map.wrapS = THREE.RepeatWrapping
+  //     materials.Granite_.map.wrapT = THREE.RepeatWrapping
+  //     materials.Granite_.map.repeat.set(5, 5)
+  //   }
+  // }, [materials])
 
   return idsNodes.map((id) => {
     const node = nodes[id]
@@ -38,9 +69,22 @@ const GLTFWalls = ({ path, showCollisions }) => {
       <Wall
         key={node.uuid}
         geometry={node.geometry}
-        material={materials.Granite_}
+        node={node}
         showCollisions={showCollisions}
-      />
+        material={materials.Granite_}
+      >
+        {/**<meshPhysicalMaterial
+          attach="material"
+          fog
+          metalness={0}
+          map={base}
+          bumpMap={bump}
+          aoMap={ao}
+          normalMap={normal}
+          roughness={1}
+          roughnessMap={rough}
+        />**/}
+      </Wall>
     )
   })
 }
--- objects3d/Scene/Scene.js
+++ objects3d/Scene/Scene.js
@@ -1,7 +1,10 @@
 import { Suspense, useCallback, useEffect, useState } from 'react'
-import { Sky, Stats, PerspectiveCamera, MapControls } from '@react-three/drei'
+import * as THREE from 'three'
+import { useThree, useLoader } from 'react-three-fiber'
+import { Stats, PerspectiveCamera, MapControls } from '@react-three/drei'
 import { Physics } from '@react-three/cannon'
 import { Controls, useControl } from 'react-three-gui'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import { useRouter } from 'next/router'
 import config from '../../config'
 import GLTFWalls from '../GLTFWalls'
@@ -79,29 +82,40 @@ const View = ({ controlsEnabled, hideControls }) => {
 }
 
 const Environment = () => {
+  const { scene } = useThree()
+  const hdri = useLoader(THREE.TextureLoader, '/hdri.jpeg')
+
+  useEffect(() => {
+    if (!hdri) return
+
+    hdri.encoding = THREE.sRGBEncoding
+    hdri.mapping = THREE.EquirectangularReflectionMapping
+    scene.background = hdri
+  }, [scene, hdri])
+
   return (
-    <>
-      <Sky
-        //
-        distance={450000}
-        sunPosition={[10, 100, 10]}
-        turbidity={8}
-        azimuth={0.25}
-        inclination={0}
-      />
-      <ambientLight intensity={0.5} color="white" position={[10, 10, -100]} />
-      <directionalLight
-        castShadow
-        position={[10, 10, 10]}
-        intensity={5}
-        shadow-camera-far={50}
-        shadow-camera-left={-10}
-        shadow-camera-right={10}
-        shadow-camera-top={10}
-        shadow-camera-bottom={-10}
-        color="blue"
-      />
-    </>
+    <group>
+      <ambientLight intensity={1} color="white" position={[10, 10, -100]} />
+      <ambientLight intensity={0} color="blue" position={[10, 10, -100]} />
+    </group>
+  )
+}
+
+const TestObj = () => {
+  // const materials = useLoader(MTLLoader, mtlUrl)
+  const object = useLoader(OBJLoader, '/gltf/TimeWarner_SA_remote.obj', (loader) => {
+    console.log(11, 'loaded', loader)
+    // materials.preload()
+    // loader.setMaterials(materials)
+  })
+
+  return (
+    <primitive
+      //
+      position={[9.8, 2, -124]}
+      scale={[0.005, 0.005, 0.005]}
+      object={object}
+    />
   )
 }
 
@@ -114,6 +128,10 @@ const Scene = () => {
         <Suspense fallback={null}>
           <Environment />
 
+          <TestObj />
+          <SampleGrass />
+          <SampleGround />
+
           <Physics>
             <Ground />
             <View controlsEnabled={controlsEnabled} hideControls={hideControls} />
@@ -124,6 +142,90 @@ const Scene = () => {
 
       {controlsEnabled && <Controls {...config.GUIControls} />}
     </Controls.Provider>
+  )
+}
+
+const SampleGround = () => {
+  const { scene } = useThree()
+
+  const [base, bump, normal, ao, rough] = useLoader(THREE.TextureLoader, [
+    '/materials/stone-floor/basecolor.jpg',
+    '/materials/stone-floor/displacement.png',
+    '/materials/stone-floor/normal.jpg',
+    '/materials/stone-floor/ambientOcclusion.jpg',
+    '/materials/stone-floor/roughness.jpg',
+  ])
+
+  useEffect(() => {
+    const repeatX = 7
+    const repeatY = 7
+
+    ;[base, bump, normal, ao, rough].forEach((texture) => {
+      texture.wrapS = THREE.RepeatWrapping
+      texture.wrapT = THREE.RepeatWrapping
+      texture.repeat.set(repeatX, repeatY)
+    })
+  }, [base, bump, normal, ao, rough])
+
+  return (
+    <mesh name="sample groud" position={[10.4, 0.1, -125]} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeGeometry attach="geometry" args={[10, 10]} />
+      <meshPhysicalMaterial
+        attach="material"
+        map={base}
+        bumpScale={10}
+        bumpMap={bump}
+        aoMapIntensity={5}
+        aoMap={ao}
+        normal={1}
+        normalMap={normal}
+        roughness={5}
+        roughnessMap={rough}
+        envMap={scene.background}
+      />
+    </mesh>
+  )
+}
+
+const SampleGrass = () => {
+  const { scene } = useThree()
+
+  const [base, bump, normal, ao, rough] = useLoader(THREE.TextureLoader, [
+    '/materials/grass-wall/basecolor.jpg',
+    '/materials/grass-wall/height.png',
+    '/materials/grass-wall/normal.jpg',
+    '/materials/grass-wall/ambientOcclusion.jpg',
+    '/materials/grass-wall/roughness.jpg',
+  ])
+
+  useEffect(() => {
+    const repeatX = 1
+    const repeatY = 1
+
+    ;[base, bump, normal, ao, rough].forEach((texture) => {
+      texture.wrapS = THREE.RepeatWrapping
+      texture.wrapT = THREE.RepeatWrapping
+      texture.repeat.set(repeatX, repeatY)
+    })
+  }, [base, bump, normal, ao, rough])
+
+  return (
+    <mesh name="sample grass" position={[10.4, 1.5, -125]}>
+      <boxGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshPhysicalMaterial
+        attach="material"
+        map={base}
+        bumpScale={1}
+        bumpMap={bump}
+        aoMapIntensity={5}
+        aoMap={ao}
+        normal={1}
+        normalMap={normal}
+        roughness={0.1}
+        roughnessMap={rough}
+        envMap={scene.background}
+      />
+    </mesh>
   )
 }
 
