1bff8d44cc91c6a38f2633b0057e6b178ffd9c03
Guilherme <guilherme.gg1@hotmail.com>
2021-06-11 17:38:54 -0300

    intro page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import Link from 'next/link'
+
 const Index = () => {
-  return <p>intro</p>
+  return (
+    <div>
+      <h1>the maze</h1>
+      <Link href="/play"><a>press Q to enter</a></Link>
+      <div>
+        novas frequencias
+        in out festival 2020
+      </div>
+    </div>
+  )
 }
 
 export default Index
