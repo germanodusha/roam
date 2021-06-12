a47a8adc6edfa94a5003ddcf1901abb63c69eae8
Guilherme <guilherme.gg1@hotmail.com>
2021-06-12 02:36:35 -0300

    update tests

--- store/index.js
+++ store/index.js
@@ -1,10 +1,43 @@
 import create from 'zustand'
 import produce, { original } from 'immer'
 
+const media = {
+  id: 1,
+  type: 'audio', // audio | video | text | track
+  title: 'title',
+  track: 'a',
+  album: '',
+  src: 'https://google.com',
+  model3d: 'something',
+}
+
 export const useStore = create((set) => {
   const setState = (fn) => set(produce(fn))
 
-  const initialState = { }
+  const initialState = {
+    player: {
+      position: [0, 0, 0],
+    },
+    movement: {
+      forward: false,
+      backwards: false,
+      left: false,
+      right: false,
+      jump: false,
+    },
+    active: false,
+    medias: [],
+    achievements: [],
+    modal: false,
+  }
 
-  return initialState
+  return {
+    state: initialState,
+    actions: {
+      reset: () => console.log('store reset'),
+      init: () => {
+        console.log('listeners go here')
+      },
+    },
+  }
 })
--- store/store.spec.js
+++ store/store.spec.js
@@ -1,39 +1,61 @@
 import { renderHook, act } from '@testing-library/react-hooks'
 import { useStore } from './'
 
-describe('useStore', () => {
+describe('test store', () => {
   let store
 
   beforeEach(() => {
-    store = renderHook(() => useStore())
+    store = renderHook(() => useStore()).result
   })
 
-  afterEach(() => {})
+  afterEach(() => {
+    act(() => { store.current.actions.reset() })
+  })
 
-  it.todo('should show intro - title')
-  it.todo('should show intro - description')
-  it.todo('should show intro - key guide')
-  it.todo('should enter the game')
+  describe('media', () => {
+    it.todo('should interact with object to main track')
+    it.todo('should close main track')
 
-  it.todo('should activate object')
-  it.todo('should disable object')
-  it.todo('should activate other object when some object already active')
+    it.todo('should interact with object to open audio')
+    it.todo('should ignore object audio')
+    it.todo('should close audio modal')
 
-  it.todo('should interact with object to main track')
-  it.todo('should close main track')
+    it.todo('should interact with object to open text')
+    it.todo('should ignore object text')
+    it.todo('should close text modal')
 
-  it.todo('should interact with object to open audio')
-  it.todo('should ignore object audio')
-  it.todo('should close audio modal')
+    it.todo('should interact with object to open video')
+    it.todo('should ignore object video')
+    it.todo('should close video modal')
+  })
 
-  it.todo('should interact with object to open text')
-  it.todo('should ignore object text')
-  it.todo('should close text modal')
+  describe('intro game', () => {
+    it.todo('should show intro - title')
+    it.todo('should show intro - description')
+    it.todo('should show intro - key guide')
+    it.todo('should enter the game')
+  })
 
-  it.todo('should interact with object to open video')
-  it.todo('should ignore object video')
-  it.todo('should close video modal')
+  describe('player movements', () => {
+    it('should move forward', () => {
+      expect(true).toBe(false)
+    })
 
-  it.todo('should open key guide')
-  it.todo('should close key guide')
+    it.todo('should move backward')
+    it.todo('should move right')
+    it.todo('should move left')
+    it.todo('should jump?')
+  })
+
+  describe('object interaction', () => {
+    it.todo('should activate object')
+    it.todo('should disable object')
+    it.todo('should activate other object when some object already active')
+  })
+
+  describe('key guide', () => {
+    it.todo('should open key guide')
+    it.todo('should close key guide')
+  })
 })
+
