de83251b238495c8cc9f8759cb61b4c9719565cb
Guilherme <guilherme.gg1@hotmail.com>
2021-06-30 21:57:55 -0300

    add loading

--- pages/play.js
+++ pages/play.js
@@ -1,13 +1,29 @@
+// import { useMemo } from 'react'
 import Hud from '@/components/Hud'
 import MediaCenter from '@/components/MediaCenter'
+import Loading from '@/components/Loading'
+// import { useStore } from '../store'
 import { Scene } from '../objects3d'
 
 const Index = () => {
+  {
+    /**
+    const { pointerLock } = useStore(({ state }) => state.game)
+
+    const showLocker = useMemo(() => {
+      console.log(pointerLock)
+      // const showLocker = pointerLock ? !pointerLock.current.isLocked : false
+    }, [pointerLock])
+  **/
+  }
+
   return (
     <>
       <Scene />
       <Hud />
+      {/**showLocker && <div>locker</div>**/}
       <MediaCenter />
+      <Loading />
     </>
   )
 }
