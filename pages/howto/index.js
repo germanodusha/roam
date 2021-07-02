e07560732a7e9b6ad219a6c8fd782a77a9c00f94
Guilherme <guilherme.gg1@hotmail.com>
2021-07-02 10:36:13 -0300

    howto page

--- pages/howto/index.js
+++ pages/howto/index.js
@@ -4,45 +4,120 @@ import Keycap from '@/components/Keycap'
 import StatusText from '@/components/StatusText'
 import styles from './Howto.module.scss'
 
+const Arrows = () => {
+  return (
+    <div className="root">
+      <div className="center up">
+        <span className="key-helper key-w">W</span>
+        <span className="key-arrow rotate90">{'<'}</span>
+      </div>
+      <div className="controls-line">
+        <div>
+          <span className="key-helper key-a">A</span>
+          <span className="key-arrow">{'<'}</span>
+        </div>
+        <div />
+        <div>
+          <span className="key-arrow">{'>'}</span>
+          <span className="key-helper key-d">D</span>
+        </div>
+      </div>
+      <div className="center down">
+        <span className="key-arrow rotate90">{'>'}</span>
+        <span className="key-helper key-s">S</span>
+      </div>
+
+      <style jsx>
+        {`
+          div {
+            position: relative;
+            padding: 0.5em 1em;
+            display: flex;
+            flex-direction: column;
+          }
+          .controls-line {
+            display: flex;
+            flex-direction: row;
+          }
+          .controls-line > div {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+          }
+          .key-arrow {
+            font-size: 4em;
+            font-weight: 900;
+          }
+          .key-helper {
+            font-size: 4em;
+            font-weight: 900;
+          }
+          .key-w {
+            transform: translateX(2.5%);
+          }
+          .key-a {
+            transform: translateX(-50%);
+          }
+          .key-s {
+            transform: translateX(2.5%);
+          }
+          .key-d {
+            transform: translateX(50%);
+          }
+          .up {
+          }
+          .down {
+          }
+          .rotate90 {
+            transform: rotate(90deg);
+          }
+          .center {
+            text-align: center;
+          }
+        `}
+      </style>
+    </div>
+  )
+}
 const Howto = () => {
   const router = useRouter()
 
   return (
     <WallBricks className={styles['howto']}>
-      <h3>
-        How to travel in <span className={styles['howto__title']}>roam</span>
-      </h3>
+      <div className={styles['howto__title']}>
+        <h3>How to travel in</h3>
+        <span>roam</span>
+      </div>
 
       <div className={styles['howto__content']}>
         <div className={styles['howto__content__section']}>
           <div className={styles['howto__content__section--inner']}>
-            <div className={styles['howto__content__section__controls']}>W</div>
-
-            <div
-              className={
-                styles[
-                  'howto__content__section__controls howto__controls--horizontal'
-                ]
-              }
-            >
-              <span>A</span>
-              <span>D</span>
-            </div>
-
-            <div className={styles['howto__content__section__controls']}>S</div>
+            <Arrows />
           </div>
 
-          <span>these are your legs</span>
+          <span className={styles['howto__description']}>
+            these are your legs
+          </span>
         </div>
 
         <div className={styles['howto__content__section']}>
           <div className={styles['howto__content__section--inner']}>
-            <span>Use your mouse to change vision</span>
-            <img src="/public/assets/images/cursor.png" />
-            <span>explore the maze in 360º</span>
+            <span className={styles['howto__content__section--bold']}>
+              use your mouse
+              <br />
+              to change
+              <br />
+              vision
+            </span>
+            <img src="/assets/images/Mouse360.png" />
+            <span className={styles['howto__content__section--desc']}>
+              explore the maze in 360º
+            </span>
           </div>
 
-          <span>this is your head</span>
+          <span className={styles['howto__description']}>
+            these are your head
+          </span>
         </div>
       </div>
 
